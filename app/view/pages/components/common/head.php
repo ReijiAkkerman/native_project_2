a9b2b971d66c4a58e1f5c5bbbcb511054e2a9d65
reijiakkerman@gmail.com <ReijiAkkerman>
2023-12-10 22:14:01 +0300

    initial commit

---
+++ app/view/pages/components/common/head.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="../../css/position.css">
+<link rel="stylesheet" href="../../css/style.css">
+<link rel="stylesheet" href="../../css/fonts.css">
+<link rel="stylesheet" href="../../css/log/position.css">
+<link rel="stylesheet" href="../../css/log/style.css">
---
+++ app/view/pages/log.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include_once __DIR__ . "/components/common/head.php" ?>
+    </head>
+    <body class="log">
+        <header>
+            
+        </heder>
+        <main>
+            
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
